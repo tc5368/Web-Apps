5b2c79c41f9d74ed80093da8d43249fe46fe9840
Tom <tc536890@gmail.com>
2019-03-21 17:31:58 +0000

    Search is Done

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
+from wtforms import StringField, IntegerField, PasswordField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from shop.models import User
 
